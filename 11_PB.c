d30659df7c7252680803c97c064a972f0f0264fa
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-10 20:08:39 +0800

    Create 11_PB.c

---
+++ 11_PB.c
@@ -0,0 +1,26 @@
+  /* USER CODE BEGIN WHILE */
+  uint32_t t0,t1;
+  t0 = t1 = HAL_GetTick();
+  while (1)
+  {
+	  if(t0 < HAL_GetTick())
+	  {
+		  t0 += 500;
+		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  }
+	  if(t1 < HAL_GetTick())
+	  {
+		  t1 += 10;
+		  uint8_t pb = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13);
+
+		  if(pb==0)	strcpy(tx2buf,"\r\nPB is pushed");
+		  if(pb==1)	strcpy(tx2buf,"\r\nPB is NOT pushed");
+
+		  HAL_UART_Transmit_IT(&huart2, (uint8_t*)tx2buf, strlen(tx2buf));
+	  }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+
