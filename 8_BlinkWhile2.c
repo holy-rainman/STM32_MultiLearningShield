a792611473fe71e4e8551126a667e3f26359e762
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-10 19:55:59 +0800

    Create 8_BlinkWhile2.c

---
+++ 8_BlinkWhile2.c
@@ -0,0 +1,19 @@
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+	  HAL_Delay(500);
+	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+	  HAL_Delay(500);
+	  
+	  while(1)
+	  {
+		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+		  HAL_Delay(100);
+	  }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+
