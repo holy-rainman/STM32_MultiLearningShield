203c97b480124faef93cf5a7152c48d87eb695df
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-10 19:52:09 +0800

    Create 6_BlinkToggle.c

---
+++ 6_BlinkToggle.c
@@ -0,0 +1,11 @@
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+	  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+	  HAL_Delay(100);
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+
