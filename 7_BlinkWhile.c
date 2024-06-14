29ac2f784566d7a48b81a269d53ceaf5d1061c09
Abd Rahim Kasiman <holy_rainman@yahoo.com>
2024-06-14 18:04:31 +0800

    Update 7_BlinkWhile.c

--- 7_BlinkWhile.c
+++ 7_BlinkWhile.c
@@ -1,11 +1,14 @@
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-	  HAL_Delay(500);
-	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-	  HAL_Delay(500);
-	  
+	  for(uint8_t i=0;i<5;i++)
+	  {
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+		  HAL_Delay(500);
+		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+		  HAL_Delay(500);
+	  }
+
 	  while(1)
 		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
     /* USER CODE END WHILE */
